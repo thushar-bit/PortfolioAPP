2c17dec73d2d7445eb842e0da19c865be5c75315
thusharnic <gsthushar44@gmail.com>
2024-05-04 18:08:12 +0530

    some major changes in the front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,68 +3,97 @@ import styles from './Component/Page.module.css'
 
 const Home: React.FC = () => {
   return (
-    <main className="flex flex-col items-center justify-between p-24 bg-black text-white min-h-screen">
-      <header className="flex flex-col items-center justify-center w-full mb-8">
-        <nav className="flex items-center">
-          <a href="#" className="mx-4">Home</a>
-          <a href="#" className="mx-4">Skills</a>
-          <a href="#" className="mx-4">Experience</a>
-          <a href="#" className="mx-4">Contact</a>
-        </nav>
-      </header>
-
-      <section className="flex items-start justify-start flex-grow">
-  <div className="text-left">
-    <h2 className="text-8xl font-extrabold mb-4 tracking-widest">
-      <span>I'</span>m<br />
-      Thushar<br />
-      G S<span className="text-blue-500">.</span>
-    </h2>
-    <h2 className="text-8xl font-extrabold mb-4 tracking-widest">
-      <span>0</span>1
-      SKILLS
-      <span className="text-blue-500">.</span>
-    </h2>
-    <nav>
-  <ul>
-    <li>
-      <a href="#wordpress">Wordpress</a>
-      <div className={styles["progress-bar"]} style={{ width: "70%" }}>
-        <span className={styles["progress-text"]}>30%</span>
-      </div>
-    </li>
-    <li>
-      <a href="#dotnet">.NET</a>
-      <div className={styles["progress-bar"]} style={{ width: "70%" }}>
-        <span className={styles["progress-text"]}>70%</span>
-      </div>
-    </li>
-    <li>
-      <a href="#javascript">JavaScript</a>
-      <div className={styles["progress-bar"]} style={{ width: "100%" }}>
-        <span className={styles["progress-text"]}>100%</span>
-      </div>
-    </li>
-  </ul>
-</nav>
-
+    <main className="flex flex-col items-center justify-center p-24 bg-black text-white min-h-screen">
+  <header>
+    <nav className="flex items-center">
+      <a href="#" className="mx-4">Home</a>
+      <a href="#" className="mx-4">Skills</a>
+      <a href="#" className="mx-4">Contact</a>
+    </nav>
+  </header>
+  <br></br>
+  <div className="text-center">
+  <div>Hi there, I'm Thushar G S.</div>
+  <div>I am a software Developer.</div>
+  <div>Welcome to my Life.</div>
+</div>
+  <section className="flex items-center justify-center flex-grow">
+    <div className="text-center">
+    <h1 style={{alignItems:"end"}}>SKILLS</h1>
+    <table style={{ borderCollapse: 'separate', borderSpacing: '30px' }}>
+  <tr>
+    <td>Python</td>
+    <td>MongoDB</td>
+    <td>Django</td>
+  </tr>
+  <tr>
+    <td>.NET MVC</td>
+    <td>.NET API</td>
+    <td>SQL (Structured Query Language)</td>
+  </tr>
+  <tr>
+    <td>Node JS</td>
+    <td>C#</td>
+    <td>OOP and Data structures</td>
+  </tr>
+  <tr>
+    <td>React JS</td>
+    <td>React Native Expo</td>
+    <td>React Native</td>
+  </tr>
+  <tr>
+    <td>MSSQL Server</td>
+    <td>.NET Web Forms</td>
+    <td>Oracle Database</td>
+  </tr>
+</table>
+    </div>
+  </section>
+  <section className="flex items-center justify-center flex-grow">
+  <div className="text-center">
+    <h1 style={{ alignItems: "end" }}>Projects</h1>
+    <table style={{ borderCollapse: 'separate', borderSpacing: '30px' }}>
+      <tr>
+        <th>Gangakalayana</th>
+        <th>Project Oversight</th>
+        <th>Some Other Project</th>
+      </tr>
+      <tr>
+        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
+          This was a govt initiated project. This description is long and will wrap within the limited width.
+        </td>
+        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
+          This was an internal project. This description is long and will wrap within the limited width.
+        </td>
+        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
+          Description of Some Other Project. This description is long and will wrap within the limited width.
+        </td>
+      </tr>
+      <tr>
+        <th>Gangakalayana</th>
+        <th>Project Oversight</th>
+        <th>Some Other Project</th>
+      </tr>
+      <tr>
+        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
+          This was a govt initiated project. This description is long and will wrap within the limited width.
+        </td>
+        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
+          This was an internal project. This description is long and will wrap within the limited width.
+        </td>
+        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
+          Description of Some Other Project. This description is long and will wrap within the limited width.
+        </td>
+      </tr>
+    </table>
   </div>
 </section>
-<section className={styles.container}>
-  <div className={styles.box}>
-    <p className={styles.paragraph}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-      Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    </p>
-  </div>
-</section>
+  <footer className="text-center">
+    <p>{new Date().getFullYear()} THUSHAR G S.</p>
+  </footer>
+</main>
 
-      <footer className="text-center">
-        <p>&copy; {new Date().getFullYear()} THUSHAR G S. All rights reserved.</p>
-      </footer>
-    </main>
+
   );
 }
 

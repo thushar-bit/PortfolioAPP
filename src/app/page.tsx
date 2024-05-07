ea92e730c53f2b4682e2ca476a5cb1ee261e4f18
thusharnic <gsthushar44@gmail.com>
2024-05-07 11:43:40 +0530

    some good changes that i am happy about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,98 +1,43 @@
-import Image from "next/image";
-import styles from './Component/Page.module.css'
 
+import styles from './Component/Page.module.css'
+import { FaGithub,FaLinkedin ,FaMailBulk   } from "react-icons/fa";
 const Home: React.FC = () => {
   return (
-    <main className="flex flex-col items-center justify-center p-24 bg-black text-white min-h-screen">
-  <header>
-    <nav className="flex items-center">
-      <a href="#" className="mx-4">Home</a>
-      <a href="#" className="mx-4">Skills</a>
-      <a href="#" className="mx-4">Contact</a>
-    </nav>
-  </header>
-  <br></br>
-  <div className="text-center">
-  <div>Hi there, I'm Thushar G S.</div>
-  <div>I am a software Developer.</div>
-  <div>Welcome to my Life.</div>
-</div>
-  <section className="flex items-center justify-center flex-grow">
-    <div className="text-center">
-    <h1 style={{alignItems:"end"}}>SKILLS</h1>
-    <table style={{ borderCollapse: 'separate', borderSpacing: '30px' }}>
-  <tr>
-    <td>Python</td>
-    <td>MongoDB</td>
-    <td>Django</td>
-  </tr>
-  <tr>
-    <td>.NET MVC</td>
-    <td>.NET API</td>
-    <td>SQL (Structured Query Language)</td>
-  </tr>
-  <tr>
-    <td>Node JS</td>
-    <td>C#</td>
-    <td>OOP and Data structures</td>
-  </tr>
-  <tr>
-    <td>React JS</td>
-    <td>React Native Expo</td>
-    <td>React Native</td>
-  </tr>
-  <tr>
-    <td>MSSQL Server</td>
-    <td>.NET Web Forms</td>
-    <td>Oracle Database</td>
-  </tr>
-</table>
-    </div>
-  </section>
-  <section className="flex items-center justify-center flex-grow">
-  <div className="text-center">
-    <h1 style={{ alignItems: "end" }}>Projects</h1>
-    <table style={{ borderCollapse: 'separate', borderSpacing: '30px' }}>
-      <tr>
-        <th>Gangakalayana</th>
-        <th>Project Oversight</th>
-        <th>Some Other Project</th>
-      </tr>
-      <tr>
-        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
-          This was a govt initiated project. This description is long and will wrap within the limited width.
-        </td>
-        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
-          This was an internal project. This description is long and will wrap within the limited width.
-        </td>
-        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
-          Description of Some Other Project. This description is long and will wrap within the limited width.
-        </td>
-      </tr>
-      <tr>
-        <th>Gangakalayana</th>
-        <th>Project Oversight</th>
-        <th>Some Other Project</th>
-      </tr>
-      <tr>
-        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
-          This was a govt initiated project. This description is long and will wrap within the limited width.
-        </td>
-        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
-          This was an internal project. This description is long and will wrap within the limited width.
-        </td>
-        <td style={{ maxWidth: '200px', wordWrap: 'break-word' }}>
-          Description of Some Other Project. This description is long and will wrap within the limited width.
-        </td>
-      </tr>
-    </table>
+    
+    <div className={styles.Home}>
+     
+  <div className={styles.MainContent}>
+    <p>
+  Hi there, I'm Thushar. I'm an 25 y/o Software Developer. I like shuffling cards and building things. I enjoy language design, web development and I live on the terminal.
+    
+  </p>
+  <div className={styles.tableContainer}>
+  <h1 className={styles.heading}>SKILLS</h1>
+  <div className={styles.table}>
+    <div className={styles.header}>Python</div>
+    <div className={styles.header}>C#</div>
+    <div className={styles.header}>React Native</div>
+    <div className={styles.header}>React Native Expo</div>
+    <div className={styles.header}>MongoDb</div>
+    <div className={styles.header}>SQL(Structured Query Language)</div>
+    <div className={styles.header}>ASP WebForms</div>
+    <div className={styles.header}>ASP .NET MVC</div>
+    <div className={styles.header}>ASP .NET API</div>
+    <div className={styles.header}>HTML5</div>
+    <div className={styles.header}>CSS</div>
+    <div className={styles.header}>Javascript</div>
   </div>
-</section>
-  <footer className="text-center">
-    <p>{new Date().getFullYear()} THUSHAR G S.</p>
-  </footer>
-</main>
-
+</div>
+<hr></hr>
+<footer className={styles.footer}>
+      <p >{new Date().getFullYear()} THUSHAR G S.
+      <a href="https://github.com/thushar-bit" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/thushar-g-s" target="_blank" rel="noopener noreferrer"><FaLinkedin/></a>
+        <a href="mailto:thushargs44@gmail.com"><FaMailBulk/></a>
+        </p>
+    </footer>
+</div>
+</div>
 
   );
 }
